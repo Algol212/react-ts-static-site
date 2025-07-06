c02bad0aaba15f19a86214393c22492093a6e0d3
Algol212 <algol212339@gmail.com>
2025-07-06 22:20:27 +0900

    Test auto deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>🎉 自動デプロイ成功！</h1>
+      <h2>GitHub ActionsからS3に反映されました。</h2>
     </div>
   );
 }
